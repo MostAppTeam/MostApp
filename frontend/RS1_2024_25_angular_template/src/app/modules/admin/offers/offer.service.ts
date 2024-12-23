31dc05e598069ccd33a3c0ef1a45e2f0937552ca
AldinaKurtovic <aldina.kurtovic@edu.fit.ba>
2024-12-24 00:47:43 +0100

    Task #20 Displaying products with options (size, color).
    Categorizing products with filtering and sorting capabilities, including specific animations

---
+++ frontend/RS1_2024_25_angular_template/src/app/modules/admin/offers/offer.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Offer } from './offer.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OfferService {
+  private apiUrl = 'http://localhost:7000/api/Offers';
+
+  constructor(private http: HttpClient) {}
+
+  getOffers(filters: { minPrice?: number; maxPrice?: number; category?: string; sortBy?: string } = {}): Observable<Offer[]> {
+    let params = new HttpParams();
+
+    if (filters.minPrice) params = params.append('minPrice', filters.minPrice.toString());
+    if (filters.maxPrice) params = params.append('maxPrice', filters.maxPrice.toString());
+    if (filters.category) params = params.append('category', filters.category);
+    if (filters.sortBy) params = params.append('sortBy', filters.sortBy);
+
+    return this.http.get<Offer[]>(this.apiUrl, { params });
+  }
+
+  createPayPalOrder(offerName: string, price: number): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/createPayPalOrder`, { offerName, price });
+  }
+}
