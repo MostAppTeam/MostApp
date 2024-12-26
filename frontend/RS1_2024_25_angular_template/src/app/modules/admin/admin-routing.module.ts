b67fee511fd2fd99ce97628090da2454ba54948a
AldinaKurtovic <aldina.kurtovic@edu.fit.ba>
2024-12-26 23:36:45 +0100

    Task #21 Lazy Loading for Angular Modules
    Implementation of the "lazy loading" technique for Angular modules to optimize performance and ensure faster application loading.

--- frontend/RS1_2024_25_angular_template/src/app/modules/admin/admin-routing.module.ts
+++ frontend/RS1_2024_25_angular_template/src/app/modules/admin/admin-routing.module.ts
@@ -1,36 +1,37 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-import {AdminLayoutComponent} from './admin-layout/admin-layout.component';
-import {DestinationComponent} from './destination/destination.component';
-import {DashboardComponent} from './dashboard/dashboard.component';
-import {ReservationComponent} from './reservation/reservation.component';
-import {AdminErrorPageComponent} from './admin-error-page/admin-error-page.component';
-import {CitiesComponent} from './cities/cities.component';
-import {CitiesEditComponent} from './cities/cities-edit/cities-edit.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DestinationComponent } from './destination/destination.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { AdminErrorPageComponent } from './admin-error-page/admin-error-page.component';
+import { CitiesComponent } from './cities/cities.component';
+import { CitiesEditComponent } from './cities/cities-edit/cities-edit.component';
 import { AttractionsComponent } from './attractions/attractions.component';
-import {ShoppingCentersComponent} from './shopping-centers/shopping-centers.component';
+import { MuseumsComponent } from './museums/museums.component';
+import { EventsComponent } from './events/event.component';
+import { OffersComponent } from './offers/offers.component';
+import { UserRecommendationsComponent } from './user-recommendations/user-recommendations.component';
+import { ShoppingCentersComponent } from './shopping-centers/shopping-centers.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: AdminLayoutComponent,
-    children: [
-      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'cities', component: CitiesComponent },
-      { path: 'cities/new', component: CitiesEditComponent },
-      { path: 'cities/edit/:id', component: CitiesEditComponent },
-      { path: 'destination', component: DestinationComponent },
-      { path: 'order', component: ReservationComponent },
-      { path: 'attractions', component: AttractionsComponent },
-      { path: '**', component: AdminErrorPageComponent },
-    ]
-  }
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'cities', component: CitiesComponent },
+  { path: 'cities/new', component: CitiesEditComponent },
+  { path: 'cities/edit/:id', component: CitiesEditComponent },
+  { path: 'destination', component: DestinationComponent },
+  { path: 'order', component: ReservationComponent },
+  { path: 'attractions', component: AttractionsComponent },
+  { path: 'museums', component: MuseumsComponent },
+  { path: 'events', component: EventsComponent },
+  { path: 'offers', component: OffersComponent },
+  { path: 'shopping-centers', component: ShoppingCentersComponent },
+  { path: 'home', component: UserRecommendationsComponent },
+  { path: '**', component: AdminErrorPageComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule {
-}
+export class AdminRoutingModule {}
